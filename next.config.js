355e06154058ef8b5e2d458342fee5e68e2024f4
Saurav Sharma <saurav@codenomad.net>
2025-04-17 11:47:02 +0530

    changes

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["codewarranty.s3.us-west-1.amazonaws.com", 'images.unsplash.com'],
+    domains: ["codewarranty.s3.us-west-1.amazonaws.com"],
 
   },
 };
